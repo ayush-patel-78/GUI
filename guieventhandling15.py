79fc8b73e08afd375af33806739cafdd6d7ea612
ayush-patel-78 <ayushkurmi97@gmail.com>
2021-12-23 00:26:47 +0530

    minor updates

--- guieventhandling15.py
+++ guieventhandling15.py
@@ -1,26 +1,24 @@
 from tkinter import *
-import sys
-def showname():
-    # 1st way 
-    # fname=e1.get()
-    # lname=e2.get()
-    # l3.config(text=fname+" "+lname)
 
-    #2nd way 
-    name=e1.get()+" "+e2.get()
-    l3.config(text=name)
+def showname():
+    fname=s1.get()
+    lname=s2.get()
+    l3.config(text=fname+" "+lname)
+    
 def fnquit():
-    # sys.exit(0)  # this will kill the whole code 
     root.destroy() # this will only destroy root window if there is another window then that will not be destroyed 
 root=Tk()
 root.geometry("250x200")
 l1=Label(root,text="First Name:")
 l2=Label(root,text="Last Name:")
-e1=Entry(root)
-e2=Entry(root)
+s1=StringVar()
+s2=StringVar()
+e1=Entry(root,textvariable=s1)
+e2=Entry(root,textvariable=s2)
 b1=Button(root,text="Show",command=showname)
 b2=Button(root,text="Quit",command=fnquit)
 l3=Label(root,textvariable="",font="Arial 14 bold")
+
 l1.grid(row=0,column=0)
 e1.grid(row=0,column=1)
 l2.grid(row=1,column=0)
@@ -30,5 +28,3 @@ b1.grid(row=3,column=0)
 b2.grid(row=3,column=1,sticky=E)
 
 root.mainloop()
-
-print("Hue hue hue hue hue ")
